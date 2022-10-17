a4db8c7040eab5c594677b88d91dd87f4fbc7738
Filipe Rodrigues <filipedfr@outlook.pt>
2022-10-17 19:04:16 +0100

    removed client docker files2

--- src/examples/basic/docker/client/run_client.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 torch_client.py --cf config/fedml_config.yaml --rank $FEDML_RANK --role client
--- src/examples/basic/docker/client/run_malicious_client.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 torch_client.py --cf config/fedml_config_malicious.yaml --rank $FEDML_RANK --role client
