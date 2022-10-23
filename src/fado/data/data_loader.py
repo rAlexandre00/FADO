002c32dc82e7946a4c286c0aabbf6d4eb0a83c62
Rodrigo Alexandre <rdrslv00@gmail.com>
2022-10-23 11:42:43 +0100

    Removed redundant data on data loading and splitting

--- src/fado/data/data_loader.py
+++ src/fado/data/data_loader.py
@@ -22,11 +22,10 @@ def read_data(train_data_dir, test_data_dir):
         train_data: dictionary of train data
         test_data: dictionary of test data
     """
-    clients = []
     train_data = {}
     test_data = {}
 
-    for i, data_dir, data_dict in zip([0, 1], [train_data_dir, test_data_dir], [train_data, test_data]):
+    for data_dir, data_dict in zip([train_data_dir, test_data_dir], [train_data, test_data]):
         data_files = os.listdir(data_dir)
         data_files = [f for f in data_files if f.endswith(".json")]
         for f in data_files:
@@ -34,10 +33,8 @@ def read_data(train_data_dir, test_data_dir):
             with open(file_path, "r") as inf:
                 cdata = json.load(inf)
             data_dict.update(cdata["user_data"])
-            if i == 0:
-                clients.extend(cdata["users"])
 
-    return clients, train_data, test_data
+    return train_data, test_data
 
 
 def batch_data(args, data, batch_size):
@@ -69,7 +66,7 @@ def load_partition_data(
         args, batch_size, train_path=os.path.join(os.getcwd(), "data", "train"),
         test_path=os.path.join(os.getcwd(), "data", "test")
 ):
-    users, train_data, test_data = read_data(train_path, test_path)
+    train_data, test_data = read_data(train_path, test_path)
 
     train_data_num = 0
     test_data_num = 0
@@ -80,17 +77,17 @@ def load_partition_data(
     test_data_global = list()
     client_idx = 0
     logging.info("loading data...")
-    for u in users:
-        if u in train_data:
-            user_train_data_num = len(train_data[u]["x"])
-            user_test_data_num = len(test_data[u]["x"])
+    for user in train_data.keys():
+        if train_data[user]:
+            user_train_data_num = len(train_data[user]["x"])
+            user_test_data_num = len(test_data[user]["x"])
             train_data_num += user_train_data_num
             test_data_num += user_test_data_num
             train_data_local_num_dict[client_idx] = user_train_data_num
 
             # transform to batches
-            train_batch = batch_data(args, train_data[u], batch_size)
-            test_batch = batch_data(args, test_data[u], batch_size)
+            train_batch = batch_data(args, train_data[user], batch_size)
+            test_batch = batch_data(args, test_data[user], batch_size)
 
             # index using client index
             train_data_local_dict[client_idx] = train_batch
--- src/fado/data/data_splitter.py
+++ src/fado/data/data_splitter.py
@@ -5,7 +5,6 @@ import os
 
 def split_data(all_data_folder, partition_data_folder, num_users):
     for t in ["train", 'test']:
-        all_users = []
         all_data = {}
         data_files = os.listdir(os.path.join(all_data_folder, t))
         # Get all json files from all_data_folder
@@ -13,28 +12,19 @@ def split_data(all_data_folder, partition_data_folder, num_users):
         for f in data_files:
             with open(os.path.join(all_data_folder, t, f), 'r') as file:
                 j = json.load(file)
-            all_users.extend(j['users'][:num_users])
             all_data.update(j['user_data'])
 
-        users = all_users[:num_users]
+        users = list(all_data.keys())
+        users.sort()
+        users = users[:num_users]
 
         for i, user_id in enumerate(users, start=1):
             os.makedirs(os.path.dirname(os.path.join(partition_data_folder, f'user_{i}', t, '')), exist_ok=True)
             # Create a dictionary with user ids and empty lists
-            # new_user_data = {k: {'x': [], 'y': []} for k in new_users}
+            user_data = {k: {} for k in users}
             # Set the corresponding user list
-            user_data = {user_id: all_data[user_id]}
+            user_data[user_id] = all_data[user_id]
             with open(os.path.join(partition_data_folder, f'user_{i}', t, 'data.json'), "w") as outfile:
-                json.dump({
-                    'users': users,
-                    'user_data': user_data
-                }, outfile)
+                json.dump({'user_data': user_data}, outfile)
 
-        # Create final file for server
-        os.makedirs(os.path.dirname(os.path.join(partition_data_folder, f'server', t, '')), exist_ok=True)
-        server_data = {user_id: all_data[user_id] for user_id in users}
-        with open(os.path.join(partition_data_folder, 'server', t, 'data.json'), "w") as outfile:
-            json.dump({
-                'users': users,
-                'user_data': all_data
-            }, outfile)
+        # The server uses the all_data_folder
