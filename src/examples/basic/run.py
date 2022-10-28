829e4282ccda843b02b945da4d762fb25e5c6288
Filipe Rodrigues <filipedfr@outlook.pt>
2022-10-28 13:15:17 +0100

    fado.data.downloader

--- src/examples/basic/run.py
+++ src/examples/basic/run.py
@@ -6,4 +6,4 @@ config_path = 'config/attack_config.yaml'
 
 args = AttackArguments(config_path)
 leaf_executor(args.dataset)
-prepare_orchestrate(config_path, args, dev=True)
+#prepare_orchestrate(config_path, args, dev=True)
