b0ee7933371ca1047a5c700d30a2afbbba46c3c9
Rodrigo Alexandre <rdrslv00@gmail.com>
2023-02-06 18:29:22 +0000

    Simplified solution to have one IP for each client

--- src/fado/docker/client-base/config_interfaces_clients.sh
+++ src/fado/docker/client-base/config_interfaces_clients.sh
@@ -9,15 +9,26 @@ create_interface() {
 	ip link set dev $1 up
 }
 
-create_route() {
-  ip route add 10."$SUBNET_TO_REACH".1.0/32 dev 10."$SUBNET".1.0 via 10."$SUBNET".0.1
-}
+create_interface 10.1.0.2
+ip route add 10."$SUBNET_TO_REACH".1.0/32 dev 10."$SUBNET".0.2 via 10."$SUBNET".0.1
 
 created_interfaces=0
 for device_ip in {1..254}.{0..255}
 do
-	create_interface 10."$SUBNET".$device_ip
-	ip route add 10."$SUBNET_TO_REACH".$device_ip/32 dev 10."$SUBNET".$device_ip via 10."$SUBNET".0.1
+  # Create new network namespace
+  ip netns add ns10."$SUBNET".$device_ip
+  # Create new virtual interface
+	ip link add link eth0 name 10."$SUBNET".$device_ip type ipvlan mode l2
+	# Move the created interface to the new network namespace
+	ip link set 10."$SUBNET".$device_ip netns ns10."$SUBNET".$device_ip
+	# Give IP to interface
+	ip netns exec ns10."$SUBNET".$device_ip ip a add dev 10."$SUBNET".$device_ip 10."$SUBNET".$device_ip/16
+	# Activate loopback and new interface
+	ip netns exec ns10."$SUBNET".$device_ip ip link set lo up
+	ip netns exec ns10."$SUBNET".$device_ip ip link set 10."$SUBNET".$device_ip up
+	# Add route to new ns
+	ip netns exec ns10."$SUBNET".$device_ip ip route add 10."$SUBNET_TO_REACH".1.0/32 dev 10."$SUBNET".$device_ip via 10."$SUBNET".0.1
+
 	((created_interfaces++==0))
 	if [[ $created_interfaces -eq N_INTERFACES ]]; then
     break
