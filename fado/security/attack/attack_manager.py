d3baf20933cd32c6580090fed5239641a80e5397
Filipe Rodrigues <filipedfr@outlook.pt>
2023-03-01 19:07:59 +0000

    changes on random attacker

--- fado/security/attack/attack_manager.py
+++ fado/security/attack/attack_manager.py
@@ -1,8 +1,6 @@
 from typing import Type
 
 from fado.cli.arguments.arguments import FADOArguments
-from fado.runner.fl.aggregate.base_aggregator import Aggregator
-from fado.runner.fl.aggregate.mean_aggregator import MeanAggregator
 from fado.security.attack.attack_base import Attack
 from fado.security.attack.model.random_attacker import RandomAttacker
 
@@ -12,7 +10,7 @@ class AttackManager:
     @classmethod
     def get_attacker(cls) -> Attack:
         args = FADOArguments()
-        if args.attacker_args.name == 'random':
+        if args.model_attack_name == 'random':
             return RandomAttacker()
         else:
             return Attack()
--- fado/security/attack/model/random_attacker.py
+++ fado/security/attack/model/random_attacker.py
@@ -1,20 +1,21 @@
 
 import logging
+import numpy as np
 from numpy import ndarray
 
 from fado.cli.arguments.arguments import FADOArguments
 from fado.security.attack.attack_base import Attack
 
 logger = logging.getLogger("fado")
-logger = logging.LoggerAdapter(logger, {'node_id': 'router'})
+logger = logging.LoggerAdapter(logger, {'node_id': 'client'})
 fado_args = FADOArguments("/app/config/fado_config.yaml")
 
 
 class RandomAttacker(Attack):
 
     def attack_model_parameters(self, model_parameters: ndarray) -> ndarray:
-        logger.info(model_parameters)
-        return model_parameters
+        logger.info(type(model_parameters[0]))
 
+        return model_parameters
 
     
