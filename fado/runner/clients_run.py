361e96ebb00cda8e9ea7b17ba7bc654663c3810a
Rodrigo Alexandre <rdrslv00@gmail.com>
2023-03-21 18:30:43 +0000

    Partitioning clients in different python scripts

--- fado/runner/clients_run.py
+++ fado/runner/clients_run.py
@@ -74,10 +74,10 @@ def main():
         time.sleep(1)
 
     for client_id in range(1, args.number_clients + 1):
-        logger.info("Starting 30 clients")
         t = Process(target=start_client, args=(client_id,), daemon=True)
         t.start()
         if client_id % 30 == 0:
+            logger.info("Started 30 clients")
             time.sleep(1)
     t.join()
     return
