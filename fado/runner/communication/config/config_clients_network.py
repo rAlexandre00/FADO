7c5d49fdb351c76134685a6db1a55943c8f23e56
Rodrigo Alexandre <rdrslv00@gmail.com>
2023-03-21 11:38:31 +0000

    FADO now supports at least 1000 clients

--- fado/runner/communication/config/config_clients_network.py
+++ fado/runner/communication/config/config_clients_network.py
@@ -1,4 +1,5 @@
 import ipaddress
+import logging
 import os
 import subprocess
 import sys
@@ -8,6 +9,7 @@ import socket
 from fado.cli.arguments.arguments import FADOArguments
 
 args = FADOArguments(os.getenv("FADO_CONFIG_PATH", default="/app/config/fado_config.yaml"))
+logger = logging.LoggerAdapter(logging.getLogger("fado"), extra={'node_id': 'clients'})
 
 
 def create_interface(ip, mask):
@@ -18,11 +20,19 @@ def create_interface(ip, mask):
 
 def create_interfaces(n):
     base_ip = ipaddress.ip_address('10.128.1.0')
-    for i in range(n+1):
-        create_interface(str(base_ip + i), 9)
+    created_interfaces = 0
+    for i in range(n + 3):
+        ip = str(base_ip + created_interfaces)
+        rule_number = 2+i
+        if 2 + i == 254:
+            continue
+        create_interface(str(base_ip + created_interfaces), 9)
         subprocess.run(
-            ['ip', 'route', 'add', 'table', f'{2 + i}', 'to', 'default', 'via', '10.128.0.1', 'dev', str(base_ip + i)])
-        subprocess.run(['ip', 'rule', 'add', 'from', f'{str(base_ip + i)}/32', 'table', f'{2 + i}', 'priority', '10'])
+            ['ip', 'route', 'add', 'table', f'{rule_number}', 'to', 'default', 'via', '10.128.0.1', 'dev', ip]) 
+        subprocess.run(['ip', 'rule', 'add', 'from', f'{ip}/32', 'table', f'{rule_number}', 'priority', '10'])
+        created_interfaces += 1
+        if created_interfaces == n + 1:
+            break
 
 
 def config():
